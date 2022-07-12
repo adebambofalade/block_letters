2e2f562971cb1fb7c2da0f7e4fe796fc33d9cda6
Ade <adebambofalade@gmail.com>
2022-07-12 01:48:11 +0100

    Create initial.py

---
+++ initial.py
@@ -0,0 +1,24 @@
+#Lord Nocturnal
+# watching stuffs up late
+
+print("L    "+ ' N    N')
+print("L    "+ ' NN   N')
+print("L    "+ ' N N  N')
+print("L    "+ ' N  N N')
+print("L    "+ ' N    N')
+print("L    "+ ' N    N')
+print("LLLLL" +' N    N') 
+
+#BB
+
+initial ='''
+bbbb    bbbb
+b   b   b   b
+b   b   b   b
+bbbb    bbbb
+b   b   b   b
+b   b   b   b
+bbbb    bbbb
+
+'''
+print(initial)
